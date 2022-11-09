da7fb998f7b1f4dd6357eb5e3f74f8135f73c0be
Riya Chauhan <96919050+reticent11@users.noreply.github.com>
2022-11-09 23:37:04 +0530

    Create super function.py

---
+++ OOPS/OOPS-2/super function.py
@@ -0,0 +1,26 @@
+# Parent class
+class Vehicle():
+    def __init__(self, make, model, fuel):
+        self.make = make
+        # private
+        self.__model = model
+        self.__fuel = fuel
+
+    def __private_method_parent(self):
+        print("This is Private")
+
+
+class Car(Vehicle):
+    # Child class
+    def __init__(self, make, model, fuel, air_conditioning, sunroof):
+
+        super(Car, self).__init__(make, model, fuel)
+
+        self.air_conditioning = air_conditioning
+        self.sunroof = sunroof
+
+
+obj1 = Car(2019, 'Tesla', 'electric', True, True)
+print(obj1.__dict__)
+print(obj1._Vehicle__private_method_parent())
+print(obj1.make)
