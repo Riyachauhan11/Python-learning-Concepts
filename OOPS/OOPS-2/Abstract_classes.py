b63cff97c4edb8d872af58aada6d3bc92887345f
Riya Chauhan <96919050+Riyachauhan11@users.noreply.github.com>
2022-11-24 00:55:25 +0530

    Update Abstract_classes.py

--- OOPS/OOPS-2/Abstract_classes.py
+++ OOPS/OOPS-2/Abstract_classes.py
@@ -25,3 +25,38 @@ myobj.get_model()
 
 # myobj1 = Vehicle() - throws an error
 ''' cause Vehicle() is a base abstract class and no implementation occurs there'''
+
+
+##########################################################################
+
+
+from abc import ABC, abstractmethod
+
+
+class TextReaderAbstract(ABC):
+
+    def __init__(self, path, filename):
+        self.path = path
+        self.filename = filename
+
+    @abstractmethod  # this is just an interface or "RULE"
+    def get_path(self):
+        pass
+
+    @abstractmethod
+    def get_filename(self):
+        pass
+
+
+class TextReader(TextReaderAbstract):
+
+    def get_path(self):
+        return self.path
+
+    def get_filename(self):
+        return self.filename
+
+
+myobj = TextReader("/users/download", 'SAMPLE.txt')
+print(myobj.get_filename())
+print(myobj.get_path())
